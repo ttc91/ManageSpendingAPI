34b847488479229313fb6f4d97600a8318bf42f3
Phúc Nguyễn <johcarter147@gmail.com>
2022-12-11 19:56:54 +0700

    new update BE by Phuc 11-12-2022

--- src/main/java/com/example/managespending/data/models/dto/request/GetTotalBetweenDate.java
+++ src/main/java/com/example/managespending/data/models/dto/request/GetTotalBetweenDate.java
@@ -1,6 +1,9 @@
 package com.example.managespending.data.models.dto.request;
 
+
 import com.example.managespending.data.models.dto.base.BaseDTO;
+import com.example.managespending.utils.enums.GetDateType;
+import com.example.managespending.utils.enums.HistoryAction;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,17 +14,14 @@ import java.io.Serializable;
 import java.util.Date;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-public class GetTransactionListByWeekDTO extends BaseDTO implements Serializable {
+public class GetTotalBetweenDate extends BaseDTO implements Serializable {
 
     private String accountUsername;
-
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    private String startDate;
-
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    private String endDate;
-
+    private String year;
+    private String month;
+    private String lastDayOfMonth;
+    private HistoryAction historyAction;
 }
